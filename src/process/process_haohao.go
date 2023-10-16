4f5d10257252f9442f82124d0f807cff85b87561
guozejia <guozejia@bingosoft.net>
2023-10-16 17:38:20 +0800

    修改key

--- src/process/process_haohao.go
+++ src/process/process_haohao.go
@@ -1,6 +1,8 @@
 package process
 
 import (
+	"strings"
+
 	core "github.com/guojia99/my-cubing-core"
 	"gorm.io/gorm"
 )
@@ -8,5 +10,8 @@ import (
 const NvHaoHaoKey = "女装"
 
 func NvHaoHao(db *gorm.DB, core core.Core, inMessage string) (outMessage string) {
+	if !strings.HasPrefix(inMessage, NvHaoHaoKey) {
+		return ""
+	}
 	return "女装浩赶紧女装"
 }
