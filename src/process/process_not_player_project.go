d572bf77a650c6a64f32486f8d0bdc79927ec6bc
guozejia <guozejia@bingosoft.net>
2023-10-24 13:58:32 +0800

    添加未未参与查询

--- src/process/process_not_player_project.go
+++ src/process/process_not_player_project.go
@@ -21,6 +21,9 @@ func GetNotPlayerProject(db *gorm.DB, core coreModel.Core, inMessage string, qq 
 		return fmt.Sprintf("`%s` 未登记，请联系浩浩登记", qq)
 	}
 
+	var player model.Player
+	_ = db.Where("id = ?", playerUser.ID).First(&player)
+
 	var contest model.Contest
 	if err := db.Where("is_end = ?", false).Where("name like ?", fmt.Sprintf("%%%s%%", "群赛")).First(&contest).Error; err != nil {
 		return "没有开启最新的群赛，请联系浩浩开启"
@@ -53,6 +56,9 @@ func GetNotPlayerProject(db *gorm.DB, core coreModel.Core, inMessage string, qq 
 			count += 1
 		}
 	}
+	if out == "" {
+		return fmt.Sprintf("%s 已参与所有项目", player.Name)
+	}
 
-	return fmt.Sprintf("%s 未参与项目如下： %s", "", out)
+	return fmt.Sprintf("%s 未参与项目如下： %s", player.Name, out)
 }
