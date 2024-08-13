f54dd2e7b876bc6070b7f5864ce58b51998f7556
guozejia <guozejia@bingosoft.net>
2024-08-13 15:56:57 +0800

    修改bug

--- pkg/process/process_random.go
+++ pkg/process/process_random.go
@@ -117,11 +117,11 @@ func (c *Random) Do(ctx context.Context, db *gorm.DB, core core.Core, inMessage 
 	lists := randomWithMsg(msg)
 
 	if len(lists) == 1 {
-		return EventHandler(out.AddSprintf("%s\n", lists[0]))
+		return EventHandler(out.AddSprintf("%s", lists[0]))
 	}
 
 	for i, l := range lists {
-		out.AddSprintf("%d. %s", i+1, l)
+		out.AddSprintf("%d. %s\n", i+1, l)
 	}
 	return EventHandler(out)
 }
