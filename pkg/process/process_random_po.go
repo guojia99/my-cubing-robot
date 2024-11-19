85e864937f93e76cd070ced6456a78972f3429b7
guozejia <guozejia@bingosoft.net>
2024-11-19 10:41:51 +0800

    [20241119] 修改po random

--- pkg/process/process_random_po.go
+++ pkg/process/process_random_po.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	core "github.com/guojia99/my-cubing-core"
 	"gorm.io/gorm"
+	"regexp"
 	"strings"
 	"sync"
 )
@@ -37,44 +38,41 @@ func (c *RandomPo) Prefix() []string { return []string{randomPoKey1, randomPoKey
 
 func (c *RandomPo) Do(ctx context.Context, db *gorm.DB, core core.Core, inMessage InMessage, EventHandler SendEventHandler) error {
 	out := inMessage.CopyOut()
-	fstring := strings.Contains(inMessage.Content, randomPoKey2)
+	fString := strings.Contains(inMessage.Content, randomPoKey2)
 	msg := ReplaceAll(inMessage.Content, "", randomPoKey2, randomPoKey1)
 
-	sl := strings.Split(msg, " ")
-	if len(sl) == 0 {
-		return EventHandler(out.AddSprintf("空空如也"))
-	}
-
-	var newList []string
-	fStr := ""
-	for _, v := range sl {
-		v = strings.TrimLeft(v, " ")
-		if len(v) == 0 {
-			continue
+	getList := func(sl []string) []string {
+		var newList []string
+		for _, v := range sl {
+			v = strings.TrimLeft(v, " ")
+			if len(v) == 0 {
+				continue
+			}
+			if v == " " {
+				continue
+			}
+			newList = append(newList, v)
 		}
-		if v == " " {
-			continue
+		newList = shuffledCopy(newList, false)
+		return newList
+	}
+
+	if !fString {
+		sl := strings.Split(msg, " ")
+		if len(sl) == 0 {
+			return EventHandler(out.AddSprintf("空空如也"))
 		}
+		return EventHandler(out.AddSprintf(getList(sl)[0]))
+	}
 
-		if fstring && len(fStr) == 0 {
-			fStr = v
-			continue
+	fRe := regexp.MustCompile(`\{([^}]+)\}`)
+	for {
+		match := fRe.FindStringSubmatch(msg)
+		if match == nil {
+			break
 		}
-
-		newList = append(newList, v)
+		options := strings.Split(match[1], " ")
+		msg = strings.Replace(msg, match[0], getList(options)[0], 1)
 	}
-	newList = shuffledCopy(newList, false)
-
-	find := newList[0]
-	if !fstring {
-		return EventHandler(out.AddSprintf(find))
-	}
-
-	if strings.Contains(fStr, "{}") {
-		fStr = strings.ReplaceAll(fStr, "{}", find)
-	} else {
-		fStr += find
-	}
-
-	return EventHandler(out.AddSprintf(fStr))
+	return EventHandler(out.AddSprintf(msg))
 }
